20f1595f531e947d7612f567d04669b0e8cb9f37
Reina-Huang <78606172+Reina-Huang@users.noreply.github.com>
2022-03-05 21:57:18 -0600

    Update Exercise 2-1_hyt.R

--- Exercise_2_r_scripts/Exercise 2-1_hyt.R
+++ Exercise_2_r_scripts/Exercise 2-1_hyt.R
@@ -13,14 +13,10 @@ avg_boarding = capmetro_UT %>%
   summarize(meanboarding = mean(boarding)) 
 avg_boarding
 
-week = mutute(capmetro_UT, weekdays = day_of_week == "Mon", "Tue", "Wed","Thu", "Fri", weekend = day_of_week == "Sat", "Sun")
-
 ggplot(avg_boarding) +
   geom_line(aes(x = hour_of_day, y=meanboarding, color=month)) +
   facet_wrap(~day_of_week) +
   labs(title="Average boardings grouped by hour of the day, day of week, and month", y="mean boarding", x="hour of day")
-
-head(capmetro_UT)
 
 # graph2
 avg_boarding_tem = capmetro_UT %>%
