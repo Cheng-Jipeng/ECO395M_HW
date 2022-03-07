f17c31fe5697ac183094eccb40c748ba7cb83bc9
wdhou <78910682+wdhou@users.noreply.github.com>
2022-03-07 00:09:51 -0600

    exercise 2

--- Exercise_2_r_scripts/Exercise 2-3_hyt.R
+++ Exercise_2_r_scripts/Exercise 2-3_hyt.R
@@ -18,5 +18,6 @@ g_c_glm = glm(formula = Default ~ duration + amount + installment + age +
 summary(g_c_glm)
 
 # What do you notice about the history variable vis-a-vis predicting defaults? 
-
 # What do you think is going on here? In light of what you see here, do you think this data set is appropriate for building a predictive model of defaults, if the purpose of the model is to screen prospective borrowers to classify them into "high" versus "low" probability of default? Why or why not---and if not, would you recommend any changes to the bank's sampling scheme?
+#模型不好，选三个相似的feature,有两个feature选入样本，有遗漏一个feature，导致错误的提高了另外两个feature的错误程度，类似于selection bias=overrepresented
+#因为我是刻意对着这两个feature找的sample
